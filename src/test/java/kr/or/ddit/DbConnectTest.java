2b60832298058ea5dcd149da828f5c49a715bfea
KMGeon <pos04167@kakao.com>
2023-01-01 17:27:52 +0900

    new year

--- src/test/java/kr/or/ddit/DbConnectTest.java
+++ src/test/java/kr/or/ddit/DbConnectTest.java
@@ -6,15 +6,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DbConnectTest {
+    // MySQL Connector 의 클래스. DB 연결 드라이버 정의
     private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
     // DB 경로
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USER = "project";
-    private static final String PASSWORD = "java";
-
+    private static final String USER = "kmg";
+    private static final String PASSWORD = "1234";
     @Test
     public void testConnection() throws Exception {
-        // DBMS에게 DB 연결 드라이버의 위치를 알려주기 위한 메소드
         Class.forName(DRIVER);
         try {
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -23,4 +22,6 @@ public class DbConnectTest {
             e.printStackTrace();
         }
     }
+
+
 }
